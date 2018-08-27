247be5ebcb4f04404107a64510a6b2c1ee39adfa
313835026 <kailewokao19820225@hotmail.com>
2018-08-27 23:08:08 +0800

    update

--- src/main/java/com/wsdll/app/DemoApplication.java
+++ src/main/java/com/wsdll/app/DemoApplication.java
@@ -2,11 +2,13 @@ package com.wsdll.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan("com.wsdll.demo.controller") 扫描包
 //@EnableAutoConfiguration() 注入spring容器
 
-// @SpringBootApplication相当于上面两个注解了
+// @SpringBootApplication相当于上面两个注解了,但只能默认扫描同目录下的包
+@ComponentScan("com.wsdll.controller")
 @SpringBootApplication
 public class DemoApplication {
 
