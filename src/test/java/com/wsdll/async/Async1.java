7092e7eaa5c27d9432120762a81f7e307f6a1293
313835026 <kailewokao19820225@hotmail.com>
2018-11-20 18:48:30 +0800

    commit

---
+++ src/test/java/com/wsdll/async/Async1.java
@@ -0,0 +1,41 @@
+package com.wsdll.async;
+
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: WH
+ * @create: 2018-11-20 18:31
+ **/
+public class Async1 implements Runnable {
+
+    StringBuilder stringBuilder;
+
+    StringBuffer stringBuffer;
+
+    @Test
+    public void demo() {
+        this.stringBuilder = new StringBuilder("stringBuilder");
+
+        this.stringBuffer = new StringBuffer("stringBuffer");
+        System.out.println(this.stringBuilder);
+        System.out.println(this.stringBuffer);
+        new Thread(new Async1()).start();
+    }
+
+//    public void run(StringBuilder stringBuilder, StringBuffer stringBuffer){
+//        stringBuilder.append("+Builder");
+//        stringBuffer.append("+Buffer");
+//        System.out.println(stringBuilder);
+//        System.out.println(stringBuffer);
+//        System.out.println("start");
+//
+//    }
+
+    @Override
+    public void run() {
+        System.out.println(this.stringBuilder);
+        System.out.println(this.stringBuffer);
+        System.out.println("start");
+    }
+}
---
+++ src/test/java/com/wsdll/instanceoff/Instanceoff.java
@@ -0,0 +1,20 @@
+package com.wsdll.instanceoff;
+
+import org.junit.Test;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+/**
+ * @description:
+ * @author: WH
+ * @create: 2018-11-20 13:06
+ **/
+public class Instanceoff {
+
+    @Test
+    public void demo() {
+        Object obj = new StringBuilder();
+
+        System.out.println(obj instanceof String);
+    }
+}
